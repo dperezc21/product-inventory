3200d9d780436c1c7304d29f91bf3f6682174559
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-01-19 23:13:52 -0500

    feat: using product inventory management deployed

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+
+// TODO: put /src/environments/* in .gitignore
+export const environment = {
+  production: false,
+  PRODUCT_INVENTORY_URL: 'https://product-inventory-management-production.up.railway.app',
+};
